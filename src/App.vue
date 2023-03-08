fd14987a6b8784209e2aad375f550e6ec004ca96
devisasari <isasari.special.mailim@gmail.com>
2023-03-08 20:52:06 +0300

    setup and test pug & scss

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,34 @@
 
-<template>
-  <div> App </div>
+<template lang="pug">
+
+h1 Hello World!
+button Click me!
+
 </template>
+
+<style lang="scss">
+
+h1 {
+  color: green;
+
+  &:hover {
+    color: greenyellow;
+  }
+}
+
+button {
+  color: red;
+  background-color: blue;
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 10px;
+  font-size: 20px;
+  cursor: pointer;
+
+  &:hover {
+    color: white;
+    background-color: green;
+  }
+}
+
+</style>
